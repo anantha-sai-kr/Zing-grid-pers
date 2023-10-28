054fe7673e8491ad3646f83d98e252e7c400dc79
anantha-sai-kr <avadhanam.ananthasai@kroger.com>
2023-10-28 20:54:37 +0530

    Added data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+/* eslint-disable no-undef */
 import './App.css';
+import 'zinggrid';
+
+ZingGrid.serLicense()
 
 function App() {
+  const data = [
+    { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>`,"count": 1, "val": 50 },
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian","count": 1, "val": 50 },
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund","count": 1, "val": 50 },
+    { "name": "Plop", "breed": `<h2>Corgi</h2>`,"count": 1, "val": 50 },
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund","count": 1, "val": 50 },
+    { "name": "Plop", "breed": `<h2>Corgi</h2>`,"count": 1, "val": 50 },
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund","count": 1, "val": 50 },
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+        { "name": `<h2>Sousage</h2>`, "breed": "Dachshund" ,"count": 1, "val": 50},
+    { "name": "Plop", "breed": `<h2>Corgi</h2>` ,"count": 1, "val": 50},
+    { "name": `<h2>Floof<h2>`, "breed": "Pomeranian" ,"count": 1, "val": 50},
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='page'>
+        <div className='table'>
+        <zing-grid
+          zebra
+          data={JSON.stringify(data)}
+          gridlines={""}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <zg-colgroup>
+              <zg-column key="count" index="count" filter="disabled" type="row-group" />
+            <zg-column
+              key="val"
+              header="name"
+              index="val"
+              filter="disabled"
+              type="number"
+              group-head-cell="avg"
+            />
+            <zg-column
+              key="breed"
+              header="breed"
+              index="breed"
+              filter="disabled"
+            />
+
+          </zg-colgroup>
+          </zing-grid>
+          </div>
+      </div>
+  )
 }
 
 export default App;
